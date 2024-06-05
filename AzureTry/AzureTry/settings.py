f12c9cf4770c331e67cf4016d649f57a5a4a0e52
Justice Ayamdoo <108245290+Justice52@users.noreply.github.com>
2024-06-05 12:28:29 +0000

    Update settings.py

--- AzureTry/AzureTry/settings.py
+++ AzureTry/AzureTry/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gq2i*a%8ha%%9lmpl@zy@r3_9l=zw5kh*l1_4x)1_1y@*amg+s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
